322aa9832886ad5e99f9cf15b47815c8ceaa928e
yuhua.li <yuhua.li@thoughtworks.com>
2022-11-09 17:08:01 +0800

    Feat: Complete chapter6 code

--- src/main/kotlin/chapter6/Primitive.kt
+++ src/main/kotlin/chapter6/Primitive.kt
@@ -1,2 +1,36 @@
 package chapter6
 
+fun main() {
+    showProgress(146)
+
+    println(PersonWithAge("Sam", 35).isOlderThan(PersonWithAge("Amy", 42)))
+    println(PersonWithAge("Sam", 35).isOlderThan(PersonWithAge("Jane")))
+
+    val i = 1
+    val l: Long = i.toLong()
+    println(l)
+    println(i.toLong() in listOf(1L, 2L, 3L))
+
+    val b: Byte = 1
+    val long = b + 1L
+    foo(long)
+    println("42".toInt())
+    // Will auto-boxing
+    val answer: Any = 42
+    println(answer)
+}
+
+fun showProgress(progress: Int) {
+    val percent = progress.coerceIn(0, 100)
+    println("We're ${percent}% done!")
+}
+
+data class PersonWithAge(val name: String, val age: Int? = null) {
+    fun isOlderThan(other: PersonWithAge): Boolean? {
+        if (age == null || other.age == null)
+            return null
+        return age > other.age
+    }
+}
+
+fun foo(l: Long) = println(l)
