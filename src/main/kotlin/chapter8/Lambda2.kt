d340330673ff2298c31fa73ab142d9a32c35ed08
yuhua.li <yuhua.li@thoughtworks.com>
2022-11-17 20:55:35 +0800

    Feat: Complete chapter8 code

--- src/main/kotlin/chapter8/Lambda2.kt
+++ src/main/kotlin/chapter8/Lambda2.kt
@@ -1,5 +1,8 @@
 package chapter8
 
+import chapter1.Person
+
+
 fun main() {
     val log = listOf(
         SiteVisit("/", 34.0, OS.WINDOWS),
@@ -11,6 +14,17 @@ fun main() {
 
     println(log.averageDurationFor { it.os in setOf(OS.ANDROID, OS.IOS) })
     println(log.averageDurationFor { it.os == OS.IOS && it.path == "/signup" })
+
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+    lookForAlice(people)
+    lookForAlice2(people)
+    lookForAlice3(people)
+
+    println(StringBuilder().apply sb@{
+        listOf(1, 2, 3).apply {
+            this@sb.append(this.toString())
+        }
+    })
 }
 
 data class SiteVisit(
@@ -21,3 +35,34 @@ enum class OS { WINDOWS, MAC, IOS, ANDROID }
 
 fun List<SiteVisit>.averageDurationFor(predicate: (SiteVisit) -> Boolean) =
     filter(predicate).map(SiteVisit::duration).average()
+
+fun lookForAlice(people: List<Person>) {
+    people.forEach {
+        if (it.name == "Alice") {
+            println("Found!")
+            // Will return, only can be used in inline function
+            return
+        }
+    }
+    println("Alice is not found!")
+}
+
+fun lookForAlice2(people: List<Person>) {
+    people.forEach label@{
+        // Will only return from lambda
+        if (it.name == "Alice") return@label
+    }
+    people.forEach {
+        // Will only return from lambda
+        if (it.name == "Alice") return@forEach
+    }
+    println("Alice might be somewhere")
+}
+
+fun lookForAlice3(people: List<Person>) {
+    // Anonymous function can return
+    people.forEach(fun(person) {
+        if (person.name == "Alice") return
+        println("${person.name} is not Alice")
+    })
+}
